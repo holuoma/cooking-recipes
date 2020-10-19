45b848873c18c3c05a12eb90a18e96007e2d144a
Henry Oluoma <70064260+holuoma@users.noreply.github.com>
2020-10-19 19:18:33 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,1 @@
-import React from 'react'
-import Navbar from './Nav'
-// import './App.css';
 
-
-const Header = ()=>{
-    return (
-       <>
-       <Navbar/>
-      </>)
-}
-
-
-export default Header
